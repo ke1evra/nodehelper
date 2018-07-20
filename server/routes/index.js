4e917c57bd6f32491f5de3de42bcda366873db88
ke1eva <dolgov.konstantin@gmail.com>
2018-07-21 00:30:07 +0300

    Добавил шаблон тест

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Приложение для добавления товаров' });
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+let data = {
+  name: "test",
+  id: "1",
+  text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('test', data);
 });
 
 module.exports = router;
